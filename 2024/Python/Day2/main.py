05c8fc2353cbe75634c96928286b0b57d106d97c
Phillip Price <trafub@gmail.com>
2024-12-08 17:14:32 -0600

    Update for part 2

--- 2024/Python/Day2/main.py
+++ 2024/Python/Day2/main.py
@@ -1,33 +1,67 @@
 DEBUG = True
 
 def checkIfSafe(myList):
-    isSafe = True
+    isTrulySafe = True
     monotonicallyIncreasing = int(myList[0]) < int(myList[1])
 
+    firstUnsafeLevel = None
     for i in range(len(myList)-1):
         difference = int(myList[i]) - int(myList[i+1])
         # adjacent levels must be different
         if difference == 0:
-            isSafe = False
+            isTrulySafe = False
+            firstUnsafeLevel = myList[i+1]
+            break
         elif monotonicallyIncreasing:
             if difference not in range(-3, 0):
-                isSafe = False
+                isTrulySafe = False
+                firstUnsafeLevel = myList[i + 1]
+                break
         else: # monotonically decreasing
             if difference not in range(1, 4):
-                isSafe = False
+                isTrulySafe = False
+                firstUnsafeLevel = myList[i + 1]
+                break
 
-    # Report is safe
-    return 1 if isSafe else 0
+    # trying again, removing first level making report unsafe
+    isPartiallySafe = True
+    if not isTrulySafe:
+        dampedReport = myList.copy()
+        dampedReport.remove(firstUnsafeLevel)
+        monotonicallyIncreasing = int(dampedReport[0]) < int(dampedReport[1])
+
+        for i in range(len(dampedReport) - 1):
+            difference = int(dampedReport[i]) - int(dampedReport[i + 1])
+            # adjacent levels must be different
+            if difference == 0:
+                isPartiallySafe = False
+                break
+            elif monotonicallyIncreasing:
+                if difference not in range(-3, 0):
+                    isPartiallySafe = False
+                    break
+            else:  # monotonically decreasing
+                if difference not in range(1, 4):
+                    isPartiallySafe = False
+                    break
+
+    #return 1 if isSafe else 0
+    return (int(isTrulySafe), int(isPartiallySafe))
 
 
 def main(input_file):
-    safeReportCount = 0
+    trulySafeReportCount = 0
+    partiallySafeReportCount = 0
     with open(input_file) as f:
         entries = (t.split(sep=' ') for t in f)
         for entry in entries:
-            safeReportCount += checkIfSafe(entry)
+            #safeReportCount += checkIfSafe(entry)
+            trulySafeIncrement, partiallySafeIncrement = checkIfSafe(entry)
+            trulySafeReportCount += trulySafeIncrement
+            partiallySafeReportCount += partiallySafeIncrement
 
-    print('Number of Safe Reports: {0}'.format(safeReportCount))
+    print('Number of Truly Safe Reports: {0}'.format(trulySafeReportCount))
+    print('Number of Partially Safe Reports: {0}'.format(partiallySafeReportCount))
 
 
 if __name__ == '__main__':
