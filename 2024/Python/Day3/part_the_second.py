2642873e88ea083e36890efdba33e7ed3950dd85
Phillip Price <trafub@gmail.com>
2024-12-15 09:44:53 -0600

    Additional correction, but overshoots target value.

--- 2024/Python/Day3/part_the_second.py
+++ 2024/Python/Day3/part_the_second.py
@@ -3,28 +3,23 @@ import re
 DEBUG = False
 
 def main(input_file):
-    #print("Do something here")
-    # Composing all separate lines into one string, as do/don't can happen between lines.
-    complete_string = ""
+    #finding multiply instructions that should be ignored in the middle of lines
+    inactive_multiplies = re.compile(r"don't\(\).*?do\(\)")
+    # if the last valid instruction in a line is don't(), want to ignore everything after that
+    trailing_inactive_multiplies = re.compile(r"don''t\(\).*")
+    multiply_regex = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
+    value_regex = re.compile(r"\d{1,3}")
+    total = 0
     with open(input_file) as f:
         for line in f:
             line = line.rstrip()
-            complete_string += line
+            active_multiplies = re.sub(inactive_multiplies, "", line)
+            active_multiplies = re.sub(trailing_inactive_multiplies, "", active_multiplies)
 
-    # input data starts with an implicit do()
-    # Taking everything between don't() and do() and replacing with nothing
-    p = re.compile(r"don't\(\).*?do\(\)")
-    active_multiplies = re.sub(p, "", complete_string)
-
-    # find all instances of mul(###,###), multiply all values, calculate and return total
-    # same problem/algorithm as part 1
-    multiply_regex = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
-    multiply_instances = multiply_regex.findall(active_multiplies)
-    total = 0
-    value_regex = re.compile(r"\d{1,3}")
-    for item in multiply_instances:
-        multiplication_values = value_regex.findall(item)
-        total += int(multiplication_values[0]) * int(multiplication_values[1])
+            multiply_instances = multiply_regex.findall(active_multiplies)
+            for item in multiply_instances:
+                multiplication_values = value_regex.findall(item)
+                total += int(multiplication_values[0]) * int(multiplication_values[1])
 
     print("Total of all valid multiplications: {0}".format(total))
 
