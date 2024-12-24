b4113bbd365e2cd7bfd00e56ed1a3c5daf06bb4e
Phillip Price <trafub@gmail.com>
2024-12-24 11:26:20 -0600

    Calculate with 3rd variation
    Still get previous result that undershoots target

--- 2024/Python/Day3/part_the_second.py
+++ 2024/Python/Day3/part_the_second.py
@@ -3,23 +3,35 @@ import re
 DEBUG = False
 
 def main(input_file):
-    #finding multiply instructions that should be ignored in the middle of lines
-    inactive_multiplies = re.compile(r"don't\(\).*?do\(\)")
-    # if the last valid instruction in a line is don't(), want to ignore everything after that
-    trailing_inactive_multiplies = re.compile(r"don''t\(\).*")
-    multiply_regex = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
-    value_regex = re.compile(r"\d{1,3}")
-    total = 0
+    # new strategy:
+    # combine all lines into one string
+    complete_string = ""
     with open(input_file) as f:
         for line in f:
             line = line.rstrip()
-            active_multiplies = re.sub(inactive_multiplies, "", line)
-            active_multiplies = re.sub(trailing_inactive_multiplies, "", active_multiplies)
+            complete_string += line
 
-            multiply_instances = multiply_regex.findall(active_multiplies)
-            for item in multiply_instances:
-                multiplication_values = value_regex.findall(item)
-                total += int(multiplication_values[0]) * int(multiplication_values[1])
+    # regex search for do() or don't() or mul(###,###) and put in a list
+    #keyword_regex = re.compile(r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)")
+    keyword_regex = re.compile(r"do\(\)|don't\(\)|mul\(\d+,\d+\)")
+    my_list = keyword_regex.findall(complete_string)
+
+    # search through list, keeping track of do()/don't() ordering. Perform multiplications as necessary. Add to total
+    do_multiplications = True
+    #value_regex = re.compile(r"\d{1,3}")
+    value_regex = re.compile(r"\d+")
+    total = 0
+    for item in my_list:
+        if item == r"don't()":
+            do_multiplications = False
+            continue
+        elif item == r"do()":
+            do_multiplications = True
+            continue
+
+        if do_multiplications:
+            multiplication_values = value_regex.findall(item)
+            total += int(multiplication_values[0]) * int(multiplication_values[1])
 
     print("Total of all valid multiplications: {0}".format(total))
 
